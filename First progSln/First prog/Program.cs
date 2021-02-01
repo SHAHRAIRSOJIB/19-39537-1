6b21acc3bc3bcce56115580c1cec1fd24bdcaf0b
Shahrair Sojib <you@example.com>
2021-02-01 10:53:07 +0600

    Frist progSln

---
+++ First progSln/First prog/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace First_prog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10, y = 20; 
+            Console.WriteLine("HELLO WORLD"+" "+ x+" "+ y);
+
+            float add=x+y;
+            Console.WriteLine("Addition=" + add);
+
+            float Sub=x-y;
+            Console.WriteLine("Subtraction=" + Sub);
+
+            float Multi = x * y;
+            Console.WriteLine("Multi=" + Multi);
+
+
+            float Div = y/x;
+          
+           Console.WriteLine("Div=" + Div);
+
+
+
+        }
+    }
+}
