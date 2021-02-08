c0486fc13e85afc52bf0c96c272254739a1f131d
Shahrair Sojib <you@example.com>
2021-02-08 10:59:46 +0600

    lab-3

--- Lab3(1) main lab/Lab3(1) main lab/Course.cs
+++ Lab3(1) main lab/Lab3(1) main lab/Course.cs
@@ -8,5 +8,38 @@ namespace Lab3_1__main_lab
 {
     class Course
     {
+        string courseName;
+        string courseCode;
+        int coursCode;
+
+        public string CourseName
+        {
+            set { courseName = value; }
+            get { return courseName; }
+        }
+        public  string CourseCode
+        {
+            set { courseCode = value; }
+            get { return courseCode; }
+        }
+        public int courseCredit
+        {
+            set { courseCredit = value; }
+            get { return courseCredit; }
+
+        }
+        public Course(string courseName, string courseCode, int courseCredit)
+        {
+            this.CourseName = courseName;
+            this.CourseCode = courseCode;
+            this.courseCredit = courseCredit;
+        }
+        public void ShowCourseInfo()
+        {
+            Console.WriteLine("Course Name: " + courseName);
+            Console.WriteLine("Course Code: " + courseCode);
+            Console.WriteLine("Course Credit: " + courseCredit);
+        }
+
     }
 }
