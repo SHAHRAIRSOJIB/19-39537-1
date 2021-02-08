4d24f3a56d1a3afc382fff44e3aa57e230530bc2
Shahrair Sojib <you@example.com>
2021-02-08 08:58:04 +0600

    Labtask-3

---
+++ Even numbers/Even numbers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            int n = 40;
+            Console.WriteLine("Even Number = " + n);
+            for (i = 1; i <= n; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+
+        }
+    }
+}
---
+++ Sum of even odd/Sum of even odd/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sum_of_even_odd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 100;
+            int i;
+            int Sum_of_even =0;
+            int Sum_of_odd =0;
+            Console.WriteLine(" Number= " + n);
+            for (i = 1; i <= n; i++)
+            {
+
+                if (i % 2 == 0)
+
+                    Sum_of_even = Sum_of_even + i;
+
+                else
+
+                    Sum_of_odd = Sum_of_odd + i;
+
+            }
+            Console.WriteLine(Sum_of_even);
+            Console.WriteLine(Sum_of_odd);
+
+        }
+    }
+}
