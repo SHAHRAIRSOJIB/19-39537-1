a40ed56f5f11de8b713f39c49647ecaee99781f2
Shahrair Sojib <you@example.com>
2021-02-08 09:32:28 +0600

    Lab

---
+++ Grading system/Grading system/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grading_system
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double Physics =80;
+            double Chemistry =85; 
+            double  Biology=90; 
+            double Mathematics =80; 
+            double Computer = 90;
+            
+           double  percentage = (Physics + Chemistry  +Biology + Mathematics + Computer )/5;
+           Console.WriteLine("Percentage = "+ percentage);
+            
+            if( percentage >= 90)
+            {
+                Console.WriteLine("Grade: A+");
+            }
+            else if(percentage  >= 85 && percentage  < 90)
+            {
+                Console.WriteLine("Grade: A");
+            }
+            else if(percentage  >= 80 && percentage  < 85)
+            {
+                Console.WriteLine("Grade: B+");
+            }
+            else if(percentage  >= 75 && percentage  < 80)
+            {
+                Console.WriteLine("Grade: B");
+            }
+            else if(percentage  >= 50 && percentage  < 75)
+            {
+                Console.WriteLine("Grade: C+");
+            }
+            else
+            {
+                Console.WriteLine("Grade: F");
+            }
+
+            Console.WriteLine("");
+}
+        }
+    }
+
---
+++ Lab3(1) main lab/Lab3(1) main lab/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3_1__main_lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
