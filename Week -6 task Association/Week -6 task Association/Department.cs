8a07c11994eab012f3e7a56fd7f812f36e4e5e8d
Shahrair Sojib <you@example.com>
2021-03-01 11:15:51 +0600

    Association Task

---
+++ Week -6 task Association/Week -6 task Association/Department.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week__6_task_Association
+{
+    class Department
+    {
+         private string id;
+        private string name;
+
+        private int studentCount; 
+        private Student[] students;
+
+        public Department()
+        {
+            students = new Student[50];
+        }
+
+        public Department(string id, string name)
+        {
+            this.Name = name;
+            this.Id = id;
+            students = new Student[100];
+        }
+
+        public string Id
+        {
+            set { this.id = value; }
+            get { return this.id; }
+        }
+
+        public string Name
+        {
+            set { this.name = value; }
+            get { return this.name; }
+        }
+
+        public int StudentCount
+        {
+            set { this.studentCount = value; }
+            get { return this.studentCount; }
+        }
+
+        public void AddStudent(params Student[] stdns)
+        {
+            foreach (Student s in stdns)
+            {
+                this.students[this.StudentCount++] = s;
+                s.Dept = this;
+            }
+        }
+
+        public void RemoveStudent(Student s)
+        {
+            
+        }
+
+        public Student GetStudent(string id)
+        {
+            
+            return null;
+        }
+
+        public void PrintStudent()
+        {
+            foreach (Student s in this.students)
+            {
+                s.ShowInfo();
+            }
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine("Department Id: {0}", this.Id);
+            Console.WriteLine("Department Name: {0}", this.Name);
+            Console.WriteLine("     ");
+        }
+    }
+}
+   
---
+++ Week -6 task Association/Week -6 task Association/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week__6_task_Association
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+      
+            Student stu1 = new Student("19-39537-1", "Munem", 3.31F);
+            Student stu2 = new Student("19-39537-2", "Al Shahrair", 3.81F);
+            Student stu3 = new Student("19-39537-3", "Sojib", 3.5F);
+
+            Department dept1 = new Department("CS", "Computer Science");
+            Department dept2 = new Department("EE","Electronic Engineering");
+
+            stu1.Dept = dept1;
+            stu2.Dept = dept2;
+            stu3.Dept = dept2;
+
+            stu1.ShowInfo();
+            stu1.ShowDepartment();
+
+            stu2.ShowInfo();
+            stu2.ShowDepartment();
+
+            stu3.ShowInfo();
+            stu3.ShowDepartment();
+
+            dept1.Id = "CSE";
+            dept1.Name = "Computer Science and Engineering";
+
+            stu1.ShowInfo();
+            stu1.ShowDepartment();
+
+
+            dept1.AddStudent(stu1);
+            dept1.AddStudent(stu2);
+
+            dept1.PrintStudent();
+        }
+    }
+}
